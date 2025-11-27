db8b0036d9bfb9e31ae846bb68ffcf25a57a71fc
mnosel00 <mateusz.nosel@student.pk.edu.pl>
2025-11-27 10:14:18 +0100

    Dodanie testów odległości Hamminga dla funkcji skrótu
    Wprowadzono interfejs `IHashFunction` oraz jego implementacje:
    - `Sha2Wrapper` (SHA-256),
    - `Sha3Wrapper` (SHA3-256, BouncyCastle),
    - `AsconWrapper` (ASCON-HASH-256, własna implementacja).
    
    Dodano klasę `Program` do testowania odległości Hamminga:
    - Generowanie losowych danych wejściowych,
    - Analiza statystyczna wyników (min, max, średnia, odchylenie, z-score),
    - Generowanie wykresów za pomocą biblioteki `ScottPlot`.
    
    Zaimplementowano algorytm ASCON-HASH-256 w pliku `AsconHash.cs`.
    
    Zaktualizowano plik projektu:
    - Dodano zależności: `Portable.BouncyCastle` i `ScottPlot`.
    
    Usunięto domyślny kod w `Program.cs` i zastąpiono go nową logiką.

---
+++ ZTTK_Lab3/ZTTK_Lab3/AsconHash.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZTTK_Lab3
+{
+    public static class AsconHash
+    {
+        public const int Rate = 8; // 64 bity = 8 bajtów
+        public const int HashSize = 32; // 256 bitów = 32 bajty
+
+        public static byte[] ComputeHash(byte[] input)
+        {
+            // Stan ASCON składa się z 5 słów 64-bitowych (x0...x4)
+            ulong x0 = 0, x1 = 0, x2 = 0, x3 = 0, x4 = 0;
+
+            // 1. Inicjalizacja (IV dla ASCON-HASH)
+            // IV: 0x00400c0000000100 (dla Ascon-Hash)
+            x0 = 0x00400c0000000100UL;
+
+            // 2. Absorpcja (Wchłanianie wiadomości)
+            int len = input.Length;
+            int offset = 0;
+
+            while (len >= Rate)
+            {
+                x0 ^= BytesToUlong(input, offset);
+                Permutation(ref x0, ref x1, ref x2, ref x3, ref x4, 12); // P12
+                offset += Rate;
+                len -= Rate;
+            }
+
+            // Padding (Dopełnienie)
+            // Dodajemy bit '1' i zera
+            ulong pad = 0x8000000000000000UL >> (0); // Upuść bajty, jeśli trzeba (uproszczone dla pełnych bloków, ale tutaj robimy ogólnie)
+
+            // Obsługa końcówki (partial block)
+            ulong lastBlock = 0;
+            for (int i = 0; i < len; i++)
+            {
+                lastBlock |= (ulong)input[offset + i] << (56 - 8 * i);
+            }
+            lastBlock |= 0x8000000000000000UL >> (8 * len);
+
+            x0 ^= lastBlock;
+
+            // Finalna permutacja P12 po paddingu
+            Permutation(ref x0, ref x1, ref x2, ref x3, ref x4, 12);
+
+            // 3. Wyciskanie (Squeezing) - generowanie skrótu
+            byte[] hash = new byte[HashSize];
+            for (int i = 0; i < HashSize; i += 8)
+            {
+                UlongToBytes(x0, hash, i);
+                Permutation(ref x0, ref x1, ref x2, ref x3, ref x4, 12);
+            }
+
+            return hash;
+        }
+
+        // Pomocnicza funkcja permutacji (rdzeń ASCON)
+        private static void Permutation(ref ulong x0, ref ulong x1, ref ulong x2, ref ulong x3, ref ulong x4, int rounds)
+        {
+            for (int i = 12 - rounds; i < 12; i++)
+            {
+                // Dodanie stałych rundy
+                x2 ^= ((ulong)(0xf0 - i * 0x10 + i * 0x1) << 56) | (ulong)(0xf0 - i * 0x10 + i * 0x1);
+
+                // Warstwa substytucji (S-box)
+                x0 ^= x4; x4 ^= x3; x2 ^= x1;
+                ulong t0 = x0, t1 = x1, t2 = x2, t3 = x3, t4 = x4;
+                t0 = ~t0; t1 = ~t1; t2 = ~t2; t3 = ~t3; t4 = ~t4;
+                t0 &= x1; t1 &= x2; t2 &= x3; t3 &= x4; t4 &= x0;
+                x0 ^= t1; x1 ^= t2; x2 ^= t3; x3 ^= t4; x4 ^= t0;
+                x1 ^= x0; x0 ^= x4; x3 ^= x2; x2 = ~x2;
+
+                // Warstwa dyfuzji (Linear diffusion)
+                x0 ^= RotateRight(x0, 19) ^ RotateRight(x0, 28);
+                x1 ^= RotateRight(x1, 61) ^ RotateRight(x1, 39);
+                x2 ^= RotateRight(x2, 1) ^ RotateRight(x2, 6);
+                x3 ^= RotateRight(x3, 10) ^ RotateRight(x3, 17);
+                x4 ^= RotateRight(x4, 7) ^ RotateRight(x4, 41);
+            }
+        }
+
+        private static ulong RotateRight(ulong value, int bits)
+        {
+            return (value >> bits) | (value << (64 - bits));
+        }
+
+        private static ulong BytesToUlong(byte[] b, int offset)
+        {
+            return ((ulong)b[offset] << 56) | ((ulong)b[offset + 1] << 48) |
+                   ((ulong)b[offset + 2] << 40) | ((ulong)b[offset + 3] << 32) |
+                   ((ulong)b[offset + 4] << 24) | ((ulong)b[offset + 5] << 16) |
+                   ((ulong)b[offset + 6] << 8) | (ulong)b[offset + 7];
+        }
+
+        private static void UlongToBytes(ulong value, byte[] b, int offset)
+        {
+            b[offset] = (byte)(value >> 56);
+            b[offset + 1] = (byte)(value >> 48);
+            b[offset + 2] = (byte)(value >> 40);
+            b[offset + 3] = (byte)(value >> 32);
+            b[offset + 4] = (byte)(value >> 24);
+            b[offset + 5] = (byte)(value >> 16);
+            b[offset + 6] = (byte)(value >> 8);
+            b[offset + 7] = (byte)(value);
+        }
+    }
+}
--- ZTTK_Lab3/ZTTK_Lab3/Program.cs
+++ ZTTK_Lab3/ZTTK_Lab3/Program.cs
@@ -1,2 +1,164 @@
-﻿ // See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Org.BouncyCastle.Crypto.Digests;
+using ScottPlot;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using ZTTK_Lab3;
+
+namespace HashAnalysis
+{
+    // --- INTERFEJSY I WRAPPERY (Te same co wcześniej) ---
+    public interface IHashFunction
+    {
+        string Name { get; }
+        byte[] ComputeHash(byte[] input);
+        int HashSizeInBits { get; }
+    }
+
+    public class Sha2Wrapper : IHashFunction
+    {
+        public string Name => "SHA-256";
+        public int HashSizeInBits => 256;
+        public byte[] ComputeHash(byte[] input) { using (var sha = SHA256.Create()) return sha.ComputeHash(input); }
+    }
+
+    public class Sha3Wrapper : IHashFunction
+    {
+        public string Name => "SHA3-256";
+        public int HashSizeInBits => 256;
+        public byte[] ComputeHash(byte[] input)
+        {
+            var digest = new Sha3Digest(256);
+            digest.BlockUpdate(input, 0, input.Length);
+            byte[] result = new byte[digest.GetDigestSize()];
+            digest.DoFinal(result, 0);
+            return result;
+        }
+    }
+
+    public class AsconWrapper : IHashFunction
+    {
+        public string Name => "ASCON-HASH-256";
+        public int HashSizeInBits => 256;
+        public byte[] ComputeHash(byte[] input) { return AsconHash.ComputeHash(input); } // Korzysta z Twojego pliku AsconHash.cs
+    }
+
+    // --- KLASA DO TESTÓW ---
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=== ROZPOCZYNAM TEST ODLEGŁOŚCI HAMMINGA ===");
+
+            // 1. Przygotowanie funkcji
+            var functions = new List<IHashFunction>
+            {
+                new Sha2Wrapper(),
+                new Sha3Wrapper(),
+                new AsconWrapper()
+            };
+
+            int sampleSize = 10000; // Zgodnie z wymaganiami [cite: 256]
+            Console.WriteLine($"Liczba próbek: {sampleSize}");
+
+            // Tabela wyników
+            Console.WriteLine("\n{0,-15} | {1,-5} | {2,-5} | {3,-10} | {4,-10} | {5,-10}",
+                "Funkcja", "Min", "Max", "Średnia", "Odchylenie", "Z-score");
+            Console.WriteLine(new string('-', 80));
+
+            foreach (var func in functions)
+            {
+                RunHammingTest(func, sampleSize);
+            }
+
+            Console.WriteLine("\nGotowe! Wykresy zostały zapisane w folderze z plikiem wykonywalnym (bin/Debug/...).");
+        }
+
+        static void RunHammingTest(IHashFunction algo, int sampleSize)
+        {
+            double[] distances = new double[sampleSize];
+            double[] xAxis = DataGen.Consecutive(sampleSize); // Oś X: 1, 2, 3...
+            Random rand = new Random();
+
+            int inputSize = 32; // 256 bitów wejścia (tak jak długość wyjścia, dobra praktyka)
+
+            for (int i = 0; i < sampleSize; i++)
+            {
+                // 1. Generuj losowy input
+                byte[] input1 = new byte[inputSize];
+                rand.NextBytes(input1);
+
+                // 2. Skopiuj i zmień DOKŁADNIE 1 losowy bit 
+                byte[] input2 = (byte[])input1.Clone();
+                int byteIndex = rand.Next(inputSize);
+                int bitIndex = rand.Next(8);
+                input2[byteIndex] ^= (byte)(1 << bitIndex);
+
+                // 3. Oblicz skróty
+                byte[] hash1 = algo.ComputeHash(input1);
+                byte[] hash2 = algo.ComputeHash(input2);
+
+               // 4. Oblicz dystans Hamminga 
+                distances[i] = CalculateHammingDistance(hash1, hash2);
+            }
+
+            // --- Analiza Statystyczna ---
+            double avg = distances.Average();
+            double min = distances.Min();
+            double max = distances.Max();
+            double sumSquares = distances.Sum(d => Math.Pow(d - avg, 2));
+            double stdDev = Math.Sqrt(sumSquares / (sampleSize - 1)); // [cite: 266, 311]
+
+            // Z-score: (Avg - Exp) / (SD / sqrt(N))
+          // Oczekiwana wartość: 50% długości skrótu = 128 bitów dla 256-bitowego hasha 
+            double expected = 128.0;
+            double zScore = Math.Abs((avg - expected) / (stdDev / Math.Sqrt(sampleSize))); // Wzór (3.1) z obrazka [cite: 266]
+
+            // Wyświetlenie w tabeli
+            Console.WriteLine("{0,-15} | {1,-5} | {2,-5} | {3,-10:F4} | {4,-10:F4} | {5,-10:F4}",
+                algo.Name, min, max, avg, stdDev, zScore);
+
+            // --- Generowanie Wykresu (ScottPlot) ---
+            var plt = new ScottPlot.Plot(800, 400);
+            plt.Title($"Test Odległości Hamminga: {algo.Name}");
+            plt.XLabel("Numer próbki");
+            plt.YLabel("Odległość Hamminga");
+
+            // Dodaj punkty (Scatter plot)
+            var scatter = plt.AddScatter(xAxis, distances);
+            scatter.LineWidth = 0; // Brak linii łączącej
+            scatter.MarkerSize = 2; // Małe kropki
+            scatter.Color = Color.Gray; // Kolor jak w artykule [cite: 272]
+
+            // Dodaj linię oczekiwaną (128)
+            var hLine = plt.AddHorizontalLine(expected);
+            hLine.Color = Color.Black;
+            hLine.LineWidth = 1;
+            hLine.LineStyle = LineStyle.Solid;
+
+            // Zapisz do pliku
+            string fileName = $"Hamming_{algo.Name}.png";
+            plt.SaveFig(fileName);
+        }
+
+        // Metoda pomocnicza do liczenia różnic bitów
+        static int CalculateHammingDistance(byte[] h1, byte[] h2)
+        {
+            int distance = 0;
+            for (int i = 0; i < h1.Length; i++)
+            {
+                byte val = (byte)(h1[i] ^ h2[i]); // XOR pokazuje różnice
+                while (val != 0)
+                {
+                    distance++;
+                    val &= (byte)(val - 1); // Algorytm Kernighana do liczenia bitów
+                }
+            }
+            return distance;
+        }
+    }
+}
